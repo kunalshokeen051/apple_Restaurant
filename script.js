9a90a940dd38e724246cc0bca8a7f47e59e8b020
Kunalshokeen <119399583+kunalshokeen051@users.noreply.github.com>
2023-01-05 00:05:38 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function order()
+{   
+    document.getElementById("orderconf").innerHTML = "Horray! order successfully placed";
+    document.getElementById("orderconf").style.border = "2px solid rgba(240,2,240,0.8)";
+    document.getElementById("intro1").style.color = "rgba(2,240,240,0.8)";
+    document.getElementById("intro2").style.color = "rgba(2,240,240,0.8)";
+    document.getElementById("head1").style.color = "rgba(240,2,240,0.8)";
+}
+
+
+    
